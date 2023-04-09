56b22036ef5e77f36a0cb9b905dfe38c3e05cf94
Paul Kilroy <paulkilroy@gmail.com>
2023-04-09 12:00:28 -0500

    firmware update code

--- src/ATEMOnAir.cpp
+++ src/ATEMOnAir.cpp
@@ -3,7 +3,8 @@
 #include <esp_wifi.h>
 #include <WiFi.h>
 
-#include <DNSServer.h>  // For captive portal DNS redirect
+#include <WebServer.h>
+#include <Update.h>
 
 #include <EEPROM.h>
 #include <ATEMmin.h>
@@ -19,17 +20,20 @@
 
 #define AP_SSID "onair-setup"
 
-#define PIN_LIGHTS 15
+#define PIN_LIGHTS 16
 
+void startupWeb();
 
 //Initialize global variables
-DNSServer dnsServer;
+WebServer server(80);
 bool WiFi_Worked = false;
 ATEMmin atemSwitcher;
 bool InSimulator = false;
 ulong pTime = 0;
-boolean lightUp = false;
+bool lightUp = false;
 bool foundATEM = false;
+bool prevLightUp = false;
+
 
 String getSSID() {
   return WiFi.SSID();
@@ -86,12 +90,13 @@ IPAddress gatewayIP() {
 void discoverATEM(const char info[]) {
   // _switcher_ctrl._udp.local
 
+  if( foundATEM ) return;
   log_i("%s ATEM IP not initialized - Atempting to auto discover atem", info);
   //int n = MDNS.queryService("switcher_ctrl", "udp");
   int n = MDNS.queryService("blackmagic", "tcp");
   if ( n == 0 ) {
     log_i("%s No ATEM services found", info);
-  } else {
+  } else if( MDNS.IP(0)[0] != 0 ) {
     // This is build for uncomplicated setups with only one ATEM.. should be fine
     log_i("%s %d ATEM service(s) found - choosing #1: %s", info, n, MDNS.IP(0).toString());
 
@@ -221,7 +226,7 @@ void WiFiEvent(WiFiEvent_t event) {
 void setup() {
   //Start Serial
   Serial.begin(115200);
-  delay(5000);
+  delay(1000);
 
   log_i("######################## Serial Started");
 
@@ -244,22 +249,22 @@ void setup() {
     // movedup WiFi.mode(WIFI_STA);
     //log_i("Attempting connection to WiFi Network name (SSID): [%d]", getSSID());
     //WiFi.begin();
-
     //delay(1000); // Wait to stabalize so I get the ETH_IP event
-
-
   } else {
     WiFi.begin("Wokwi-GUEST", "", 6);
   }
 
   pinMode(PIN_LIGHTS, OUTPUT);
 
+  startupWeb();
+
   log_i("Done");
 }
 
 void loop() {
-
   if (  WiFiGenericClass::getStatusBits() & ETH_HAS_IP_BIT ) {
+    server.handleClient();
+
     if ( !foundATEM ) {
       discoverATEM("LOOP");
     } else {
@@ -268,21 +273,97 @@ void loop() {
 
     if ( atemSwitcher.getStreamStreaming() ) {
       // light up
-      digitalWrite(PIN_LIGHTS, HIGH);
+      //Serial.printf("changing light status: %d\n", 1);
+      digitalWrite(PIN_LIGHTS, LOW);
+      lightUp = true;
+      if( prevLightUp != lightUp ) Serial.println("changing light status: on");
     } else if ( atemSwitcher.getStreamConnecting() || atemSwitcher.getStreamStopping() ) {
       // Blink
       if ( millis() - pTime > 200 ) {
         lightUp = !lightUp;
-        Serial.printf("changing light status bbb: %d\n", lightUp);
-        digitalWrite(PIN_LIGHTS, lightUp ? HIGH : LOW);
+        Serial.printf("changing light status: %s\n", lightUp?"on":"off");
+        digitalWrite(PIN_LIGHTS, lightUp ? LOW : HIGH);
         pTime = millis();
       }
     } else {
-      digitalWrite(PIN_LIGHTS, LOW);
+      //Serial.printf("changing light status: %d\n", 0);
+      digitalWrite(PIN_LIGHTS, HIGH);
+      lightUp = false;
+      if( prevLightUp != lightUp ) Serial.println("changing light status: off");
     }
   } else {
-    digitalWrite(PIN_LIGHTS, LOW);
+    digitalWrite(PIN_LIGHTS, HIGH);
+    lightUp = false;
   }
+  prevLightUp = lightUp;
 }
 
+const char* firmwareIndex =
+"<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>"
+"<form method='POST' action='#' enctype='multipart/form-data' id='upload_form'>"
+"<input type='file' name='update'>"
+"<input type='submit' value='Update'>"
+"</form>"
+"<div id='prg'>progress: 0%</div>"
+"<script>"
+"$('form').submit(function(e){"
+"e.preventDefault();"
+"var form = $('#upload_form')[0];"
+"var data = new FormData(form);"
+" $.ajax({"
+"url: '/update',"
+"type: 'POST',"
+"data: data,"
+"contentType: false,"
+"processData:false,"
+"xhr: function() {"
+"var xhr = new window.XMLHttpRequest();"
+"xhr.upload.addEventListener('progress', function(evt) {"
+"if (evt.lengthComputable) {"
+"var per = evt.loaded / evt.total;"
+"$('#prg').html('progress: ' + Math.round(per*100) + '%');"
+"}"
+"}, false);"
+"return xhr;"
+"},"
+"success:function(d, s) {"
+"console.log('success!')"
+"},"
+"error: function (a, b, c) {"
+"}"
+"});"
+"});"
+"</script>";
 
+void startupWeb() {
+  server.on("/firmware", HTTP_GET, []() {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/html", firmwareIndex);
+  });
+  /*handling uploading firmware file */
+  server.on("/update", HTTP_POST, []() {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
+    ESP.restart();
+  }, []() {
+    HTTPUpload& upload = server.upload();
+    if (upload.status == UPLOAD_FILE_START) {
+      Serial.printf("Update: %s\n", upload.filename.c_str());
+      if (!Update.begin(UPDATE_SIZE_UNKNOWN)) { //start with max available size
+        Update.printError(Serial);
+      }
+    } else if (upload.status == UPLOAD_FILE_WRITE) {
+      /* flashing firmware to ESP*/
+      if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
+        Update.printError(Serial);
+      }
+    } else if (upload.status == UPLOAD_FILE_END) {
+      if (Update.end(true)) { //true to set the size to the current progress
+        Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
+      } else {
+        Update.printError(Serial);
+      }
+    }
+  });
+  server.begin();
+}
